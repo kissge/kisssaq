e917f5fcf449fb1de631dbb6029bed509c745afe
Yusuke Kido <mail@yusuk.eki.do>
2026-06-02 18:30:54 +0900

    Set up PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', manifest: {
+        name: 'kissSaq',
+        short_name: 'kissSaq',
+        description: '自作クイズ管理ツールkissSaq',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   base: process.env.NODE_ENV === 'development' ? '' : '/kisssaq'
 })
